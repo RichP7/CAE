c7bc02e209cba1f3e30ae9dba8b44e8d2358075f
RichP7 <richa.parsai@consultadd.com>
2015-10-07 03:55:54 -0400

    Richa:Pages setup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+	
+  def home
+  end
+  def Our_Niche
+  end
+  def Team
+  end
+  def Live_projects
+  end
+  def Sucess_Stories
+  end
+  def Trainings
+  end
+  def Careers
+  end
+end
